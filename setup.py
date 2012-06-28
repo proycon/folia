10ff56de8d663395ac640d76eeae4d704a0c80d4
Maarten van Gompel <proycon@anaproy.nl>
2012-06-28 17:00:27 +0200

    update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "FoLiA-tools",
-    version = "0.8.2.3",
+    version = "0.8.2.4",
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("FoLiA-tools contains various Python-based command line tools for working with FoLiA XML (Format for Linguistic Annotation)"),
