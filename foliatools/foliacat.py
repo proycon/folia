b102473b46175f221e832ee0623b36f178e19f32
Maarten van Gompel <proycon@anaproy.nl>
2014-12-08 15:10:51 +0100

    added foliacat

---
+++ foliatools/foliacat.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+import getopt
+import sys
+try:
+    from pynlpl.formats import folia
+except:
+    print("ERROR: pynlpl not found, please obtain PyNLPL from the Python Package Manager ($ sudo easy_install pynlpl) or directly from github: $ git clone git://github.com/proycon/pynlpl.git",file=sys.stderr)
+    sys.exit(2)
+
+def usage():
+    print("foliacat",file=sys.stderr)
+    print("  by Maarten van Gompel (proycon)",file=sys.stderr)
+    print("  Radboud University Nijmegen",file=sys.stderr)
+    print("  2014 - Licensed under GPLv3",file=sys.stderr)
+    print("",file=sys.stderr)
+    print("Concatenates multiple FoLiA documents into one; provided that all IDs are unique.",file=sys.stderr)
+    print("",file=sys.stderr)
+    print("Usage: foliacat [options] file1 file2 file3 ... ",file=sys.stderr)
+    print("",file=sys.stderr)
+    print("Options:",file=sys.stderr)
+    print("  -o [file]                    Output file",file=sys.stderr)
+    print("  -i [id]                      ID for output file (mandatory)",file=sys.stderr)
+
+
+def concat(target, source):
+    merges = 0
+    for e in source:
+        c = e.copy(target.doc)
+        target.append(c)
+        merges += 1
+    return merges
+
+
+
+def foliacat(id, outputfile, *files):
+    totalmerges = 0
+    outputdoc = folia.Document(id=id)
+    text = folia.append(folia.Text, id=id + ".text")
+    for i, filename in enumerate(files):
+        merges = 0
+        print("Processing " + filename, file=sys.stderr)
+        inputdoc = folia.Document(file=filename)
+        print("(merging document)",file=sys.stderr)
+
+        for annotationtype,set in inputdoc.annotations:
+            if not outputdoc.declared(annotationtype,set):
+                outputdoc.declare( annotationtype, set)
+
+        for d in inputdoc.data:
+            merges += concat(text, d)
+
+        print("(merged " + str(merges) + " elements, with all elements contained therein)",file=sys.stderr)
+        totalmerges += merges
+
+    print("(TOTAL: merged " + str(totalmerges) + " elements, with all elements contained therein)",file=sys.stderr)
+    if outputfile and merges > 0:
+        outputdoc.save(outputfile)
+
+    return outputdoc
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "o:ih", ["help"])
+    except getopt.GetoptError as err:
+        print(str(err),file=sys.stderr)
+        usage()
+        sys.exit(2)
+
+    outputfile = None
+    substitute = False
+
+    for o, a in opts:
+        if o == '-h' or o == '--help':
+            usage()
+            sys.exit(0)
+        elif o == '-o':
+            outputfile = a
+        elif o == '-i':
+            folia = True
+        else:
+            raise Exception("No such option: " + o)
+
+    if len(args) < 2:
+        print("ERROR: At least two files need to be specified",file=sys.stderr)
+        sys.exit(2)
+
+    if substitute:
+        outputfile = args[0]
+
+    outputdoc = foliacat(id, outputfile, *args)
+    if not outputfile:
+        xml = outputdoc.xmlstring()
+        if sys.version < '3':
+            if isinstance(xml,unicode):
+                print(xml.encode('utf-8'))
+            else:
+                print(xml)
+        else:
+            print(xml)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "FoLiA-tools",
-    version = "0.11.1.24",
+    version = "0.11.1.25",
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("FoLiA-tools contains various Python-based command line tools for working with FoLiA XML (Format for Linguistic Annotation)"),
@@ -43,6 +43,7 @@ setup(
             'foliaquery = foliatools.foliaquery:main',
             'foliatextcontent = foliatools.foliatextcontent:main',
             'dcoi2folia = foliatools.dcoi2folia:main',
+            'foliacat = foliatools.foliacat:main',
         ]
     },
     #include_package_data=True,
