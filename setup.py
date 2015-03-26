340e6105934301e5d8a5d79b015b43003ae5467d
Maarten van Gompel <proycon@anaproy.nl>
2015-03-26 22:05:28 +0100

    Added phonetics data to example.xml , regenerated RelaxNG schema , documentation update to reflect new implementation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "FoLiA-tools",
-    version = "0.11.3.28",
+    version = "0.12.0.29",
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("FoLiA-tools contains various Python-based command line tools for working with FoLiA XML (Format for Linguistic Annotation)"),
@@ -51,5 +51,5 @@ setup(
     },
     #include_package_data=True,
     package_data = {'foliatools': ['*.xsl'] },
-    install_requires=['pynlpl >= 0.7.2', 'lxml >= 2.2','docutils']
+    install_requires=['pynlpl >= 0.7.4', 'lxml >= 2.2','docutils']
 )
