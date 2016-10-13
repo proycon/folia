9b959d70d1363a40064fa7ae10584225342fb355
Maarten van Gompel <proycon@anaproy.nl>
2016-10-13 13:06:04 +0200

    updated

--- foliatools/foliasetdefinition.py
+++ foliatools/foliasetdefinition.py
@@ -41,10 +41,12 @@ def printclass(classinfo, args, indent):
 def main():
     parser = argparse.ArgumentParser(description="A tool to read FoLiA Set Definitions and perform some operations on them. By default it will print all sets and classes. This tool can also convert from legacy XML to RDF.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--basenamespace', type=str,help="Base RDF namespace to use when converting from legacy XML to RDF", action='store',default="",required=False)
-    parser.add_argument('--rdfttl', type=str,help="Output RDF in Turtle", action='store_bool',required=False)
-    parser.add_argument('--rdfxml', type=str,help="Output RDF in XML", action='store_bool',required=False)
-    parser.add_argument('--json', type=str,help="Output set definition in JSON", action='store_bool',required=False)
-    parser.add_argument('--outputuri', type=str,help="Output full URIs in text output", action='store_bool',required=False)
+    parser.add_argument('--rdfttl', help="Output RDF in Turtle", action='store_bool',required=False)
+    parser.add_argument('--rdfxml',help="Output RDF in XML", action='store_bool',required=False)
+    parser.add_argument('--json', help="Output set definition in JSON", action='store_bool',required=False)
+    parser.add_argument('--outputuri',help="Output full RDF URIs in text output", action='store_bool',required=False)
+    parser.add_argument('--class', type=str,help="Test for the specified class, by ID", action='store',required=False)
+    parser.add_argument('--subset', type=str,help="Test for the specified subset (--class will be interpreted relative to subset then)", action='store',required=False)
     parser.add_argument('url', nargs='?', help='URL or filename to a FoLiA Set Definition')
 
     args = parser.parse_args()
@@ -54,28 +56,28 @@ def main():
     if args.rdfttl:
         print(str(setdefinition.graph.serialize(None, 'turtle',base=setdefinition.basens),'utf-8') )
     elif args.rdfxml:
-        print(str(setdefinition.graph.serialize(None, 'turtle',base=setdefinition.basens),'utf-8') )
+        print(str(setdefinition.graph.serialize(None, 'xml',base=setdefinition.basens),'utf-8') )
     elif args.json:
         json.dumps(setdefinition.json())
     else:
         #default visualization
-        seturi, setid, setlabel = setdefinition.mainset()
+        setinfo = setdefinition.mainset()
         if args.outputuri:
-            printuri = " <" + seturi + ">"
+            printuri = " <" + setinfo['uri'] + ">"
         else:
             printuri = ""
-        print("SET " + setid + printuri + ": " + setlabel)
-        for classinfo in setdefinition.classes(seturi, nestedhierarchy=True):
+        print("SET " + setinfo['id'] + printuri + ": " + setinfo['label'])
+        for classinfo in setdefinition.classes(setinfo['uri'], nestedhierarchy=True):
             printclass(classinfo, args, "  ")
         print()
 
-        for subseturi, subsetid, subsetlabel in setdefinition.subsets():
+        for subsetinfo in setdefinition.subsets():
             if args.outputuri:
-                printuri = " <" + subseturi + ">"
+                printuri = " <" + subsetinfo['uri'] + ">"
             else:
                 printuri = ""
-            print("SUBSET " + subsetid + printuri + ": " + subsetlabel)
-            for classinfo in setdefinition.classes(subseturi, nestedhierarchy=True):
+            print("SUBSET " + subsetinfo['id'] + printuri + ": " + subsetinfo['label'])
+            for classinfo in setdefinition.classes(subsetinfo['uri'], nestedhierarchy=True):
                 printclass(classinfo, args, "  ")
             print()
 
