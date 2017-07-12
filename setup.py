db95022ec567beba9b6a8b1901dd09aa338b7fa0
Maarten van Gompel <proycon@anaproy.nl>
2017-07-12 12:06:02 +0200

    Expanded allowed types under Event

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "FoLiA-tools",
-    version = "1.4.1.55",
+    version = "1.4.2.55",
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("FoLiA-tools contains various Python-based command line tools for working with FoLiA XML (Format for Linguistic Annotation)"),
