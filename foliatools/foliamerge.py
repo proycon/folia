1402003e1ac438aad5814f93c1edeff3fb1ec5bb
Maarten van Gompel <proycon@anaproy.nl>
2017-07-21 12:34:32 +0200

    foliamerge: some sanity checks

--- foliatools/foliamerge.py
+++ foliatools/foliamerge.py
@@ -67,6 +67,8 @@ def mergechildren(parent, outputdoc, asalternative):
                         alt = newparent.append(folia.Alternative, generate_id_in=newparent)
                         reID(newparent.doc, e, newparent.id, alt.id)
                         alt.append(e)
+                        assert e.parent is alt
+                        assert e.doc is outputdoc
                         alt.merged = True
                         e.merged = True
                         merges += 1
@@ -75,6 +77,8 @@ def mergechildren(parent, outputdoc, asalternative):
                         alt = newparent.append(folia.AlternativeLayers, generate_id_in=newparent)
                         reID(newparent.doc, e, newparent.id, alt.id)
                         alt.append(e)
+                        assert e.parent is alt
+                        assert e.doc is outputdoc
                         alt.merged = True
                         e.merged = True
                         merges += 1
@@ -87,6 +91,8 @@ def mergechildren(parent, outputdoc, asalternative):
                 else:
                     print("Adding Annotation type " + e.__class__.__name__ + ", set " + str(e.set) + " to " + newparent.id, file=sys.stderr)
                     newparent.append(e)
+                    assert e.parent is newparent
+                    assert e.doc is outputdoc
                     e.merged = True
                     merges += 1
         elif isinstance(e, folia.AbstractElement):
