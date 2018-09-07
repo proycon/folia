b9c9b9f1f500aff8a90de8b7fd2ab0495185e0e2
Maarten van Gompel <proycon@anaproy.nl>
2018-09-07 10:46:27 +0200

    added a new test, validation against the RelaxNG schema only (foliatools and foliapy do their own more involved validations)

---
+++ test/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FAILURE=0
+for f in examples/*.folia.xml; do
+    echo "Validating $(basename $f) against RelaxNG schema..."
+    xmllint --schema schemas/folia.rng $f > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "...FAILED" >&2
+        FAILURE=1
+    else
+        echo "...OK" >&2
+    fi
+done
+
+exit $FAILURE
